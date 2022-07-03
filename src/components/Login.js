ad56886fd1089a3a1199e1caf2dfac6c74ed93cb
abhinand <abhinandazokdio@gmail.com>
2022-07-03 22:08:46 +0530

    updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import { Grid, Paper, Avatar, TextField, Button, } from '@material-ui/core';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import React from "react";
+import { Grid, Paper, Avatar, TextField, Button } from "@material-ui/core";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 const Login = () => {
-    const paperStyle = { padding: 20, height: '70vh', width: 280, margin: '20px auto' }
-    const avatarStyle = { backgroundColor: '#1bbd7e' }
+    const [name, setName] = React.useState(null);
+    const [password, setPassword] = React.useState(null);
+    const paperStyle = {
+        padding: 20,
+        height: "70vh",
+        width: 280,
+        margin: "20px auto",
+    };
+    const avatarStyle = { backgroundColor: "#1bbd7e" };
     const loginFunction = () => {
-        alert('login Success')
-    }
+        alert("login Success");
+    };
+
+    const changeName = (x) => {
+        setName(x);
+    };
+    const changePassword = (x) => {
+        setPassword(x);
+    };
     return (
         <Grid>
             <Paper elevation={10} style={paperStyle}>
@@ -18,23 +32,42 @@ const Login = () => {
                     </Avatar>
                     <h2>Sign In</h2>
                 </Grid>
-                <TextField label='Username' placeholder='Enter your Username' fullWidth required />
-                <TextField label='Password' placeholder='Enter your Password' fullWidth required type='password' />
+                <TextField
+                    label='Username'
+                    placeholder='Enter your Username'
+                    fullWidth
+                    required
+                    value={name}
+                    onChange={(x) => changeName(x)}
+                />
+                <TextField
+                    label='Password'
+                    placeholder='Enter your Password'
+                    fullWidth
+                    required
+                    type='password'
+                    onChange={(x) => changePassword(x)}
+                />
 
                 <FormControlLabel
-                    control={
-
-                        <Checkbox
-                            name="CheckB"
-                            color="Primary"
-                        />
-                    }
-                    label="Remember me"
+                    control={<Checkbox name='CheckB' color='Primary' />}
+                    label='Remember me'
                 />
-                <Button type='submit' color="primary" variant='contained' fullWidth onClick={() => loginFunction()}>Sign in</Button>
+                <Button
+                    type='submit'
+                    color='primary'
+                    variant='contained'
+                    fullWidth
+                    onClick={() => loginFunction()}>
+                    Sign in
+                </Button>
+                <Grid />
+                <Button color='primary' variant='text' fullWidth>
+                    Sign up
+                </Button>
             </Paper>
         </Grid>
-    )
-}
+    );
+};
 
 export default Login;
